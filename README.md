47158996c8a2cc77ddfe7c2f99de29b5bb28228d
TobyMWilliams <tobyw3445@gmail.com>
2025-11-04 14:29:59 +0000

    Rest bay conditions and README update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-In this project, my aim is to create an accurate, reliable surf forecast for Rest Bay beach in Porthcawl 
+In this project, my aim is to create an accurate, reliable surf forecast for Rest Bay beach in Porthcawl:
+    This project will be built such that it is extensible to other beaches, provided the correct data is input
+
+    this project will consider both wind, and swell data, provided by public APIs 
+
+    
+This project is a working prototype of a C or C++ implementation ( I'm yet undecided)
--- REST_BAY.py
+++ REST_BAY.py
@@ -17,7 +17,26 @@ REST_BAY_COORDINATES = {
 
 # Preferable conditions for surfing at Rest Bay:
     # - Optimal Tide: Mid to High Tide
-    # - Best Swell Direction: Southwest (SW)
-    # - Ideal Wind Direction: Offshore winds from the Northeast (NE)
-    # - Suitable Wave Height: 1 to 4 feet
+    # - Best Swell Direction: Northwest (NW) Southwest (SW)
+    # - Ideal Wind Direction: Offshore winds from the [East (E) -Northeast (NE)]
+    # - Suitable Wave Height: 2 to 7 feet
+
+
+REST_BAY_PREFERABLE_CONDITIONS = {
+    "optimal_tide": ["low", "low-mid", "mid", "mid-high"],
+    "optimal_tide_direction": "push",
+
+    "ideal_swell_dir_min_deg": 225,
+    "ideal_swell_dir_max_deg": 315,
+    
+    "suitable_wave_period_min": 8,
+    "suitable_wave_period_max": 15,
+    "suitable_swell_height_min": 2,
+    "suitable_swell_height_max": 7,
+
+    "ideal_wind_direction": [45 .. 90],  # degrees
+    "suitable_wind_strength_min": 2,
+    "suitable_wind_strength_max": 15,
+}
+
     
